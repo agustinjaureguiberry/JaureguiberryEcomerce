6a7722c6c9028957643a8ae92b3d0cabcc23c4e2
agustinjaureguiberry <aaguus.mdq92@gmail.com>
2022-06-22 23:39:50 -0300

    Agrego eventos

--- assets/js/creacionHTML.js
+++ assets/js/creacionHTML.js
@@ -2,22 +2,6 @@
 
 let modificaHTML = document.body
 
-let element = document.createElement("div")
-element.id = "divContainer"
-element.className = "contentGrid"
-modificaHTML.append(element)
-element.innerHTML = "<header><img src='./assets/img/imagen.webp' alt='Imagen de e-commerce'></header>"
-
-modificaHTML = document.getElementById("divContainer")
-
-element = document.createElement("main")
-modificaHTML.append(element)
-element.innerHTML = `<h1>Proyecto final</h1>
-                    <h2></h2>
-                    <ul id="temasVistos"></ul>
-                    <img src="./assets/img/construccion.jpg" alt="Imagen de pagina en">
-                    <h2>Proximamente</h2>`
-
 modificaHTML = document.getElementById("temasVistos")
 
 temasAplicados.forEach((tema) => {
@@ -27,8 +11,6 @@ temasAplicados.forEach((tema) => {
     modificaHTML.append(li)
 })
 
-modificaHTML = document.getElementById("divContainer")
 
-element = document.CreateElement("footer")
-modificaHTML.append(element)
-element.innerHTML = `<h2>Disculpe las molestias</h2>`
+
+
--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -49,13 +49,14 @@ const agregaCuota = (array, cod, desc, precio) => {
 // -----------------------  Creacion de funciones necesarias -----------------------
 
 const muestraProd = (array) => {
-    console.log("-----------------------------")
-    console.log("      Lista de precios: ")
-    console.log("#  -   DESCRPCION   -  precio")
-    for (let i = 0; i < array.length; i++) {
-        array[i].muestra()
-    }
-    console.log("-----------------------------")
+    modificaHTML = document.getElementById("listaDePrecios")
+    array.forEach((producto) => {
+        const element = document.createElement("tr")
+        element.innerHTML = `<th>${producto.cod}</th><th>${producto.descripcion}</th><th>${producto.precio}</th>`
+        console.log(element)
+        modificaHTML.append(element)
+    });
+
 }
 
 const precioProd = (num) => {
@@ -118,91 +119,106 @@ let error = false
 let Continue = true
 
 
-alert("Bienvenido usuario al desafio N°2 correspondiente a la clase N°6")
-alert("Este simulador es ingresar productos en en una lista de precios, luego podra (si desea) generar ventas en cuotas")
-alert("Agreguemos productos entonces")
 
-do {
-    cod = parseInt(prompt("Ingrese el codigo del producto a cargar:"))
-    error = arrayProductos.some((elem) => elem.cod === cod)
-    console.log(error)
-    while (error) {
-        alert("EL CODIGO YA EXISTE - VUELVA A INTENTARLO")
-        cod = parseInt(prompt("Ingrese el codigo del producto a cargar:"))
-        error = arrayProductos.some((elem) => elem.cod === cod)
-    }
-    desc = prompt("Ingrese el nombre o descripcion del producto: ")
-    prec = parseInt(prompt("Ingrese el precio del producto: "))
-    agregaproducto(arrayProductos, cod, desc, prec)
-    console.clear()
+let btnCargar = document.querySelector("#btnCargar")
+
+
+btnCargar.addEventListener("click", () => {
+    cod = document.querySelector("#productoId")
+    desc = document.querySelector("#descripcionId")
+    prec = document.querySelector("#precioId")
+    agregaproducto(arrayProductos, cod.value, desc.value, prec.value)
     muestraProd(arrayProductos)
-    alert("PRODUCTO CARGADO")
-    sigue = prompt("Ingrese 's' para agregar otro producto o cualquier letra para finalizar.")
-    if ((sigue == "s") || (sigue == "S")) {
-        repeat = true
-    }
-    else {
-        repeat = false
-    }
-} while (repeat)
+})
 
-alert("Su lista de precios ha sido cargada, en consola aparece completa.")
-console.clear()
-muestraProd(arrayProductos)
-Continue = prompt("Ingrese la letra S si desea continuar con el programa de ventas.")
-if ((Continue == "s") || (Continue == "S")) {
-    agregaCuota(arrayCuotas, 1, "01 Cuota", 0)
-    agregaCuota(arrayCuotas, 3, "03 Cuotas", 12)
-    agregaCuota(arrayCuotas, 6, "06 Cuotas", 24)
-    agregaCuota(arrayCuotas, 9, "09 Cuotas", 36)
-    agregaCuota(arrayCuotas, 12, "12 Cuotas", 48)
-    agregaCuota(arrayCuotas, 18, "18 Cuotas", 72)
-    do {
-        console.clear()
-        muestraProd(arrayProductos)
-        alert("En la consola figura la lista de productos, el codigo y el precio del mismo.")
-        cod = parseInt(prompt("Ingrese el codigo del producto que desea comprar."))
-        error = !(arrayProductos.some((elem) => elem.cod === cod))
-        while (error) {
-            alert("ERROR, ESE CODIGO NO EXISTE. VUELVA A INTENTARLO")
-            cod = parseInt(prompt("Ingrese el codigo del producto que desea comprar."))
-            error = !(arrayProductos.some((elem) => elem.cod === cod))
-        }
-        producto = arrayProductos.find((elem) => elem.cod === cod)
-        alert("Usted ha elegido: " + producto.descripcion + " a un precio de : $" + producto.precio + ".")
-        cantidad = parseInt(prompt("Ingrese la cantidad que desea:"))
-        PrecioEfec = (producto.precio * cantidad)
-        alert("El precio final es de: $" + PrecioEfec + ".")
-        muestraCuotas(arrayCuotas)
-        cod = parseInt(prompt("Ingrese la cantidad de cuotas que desea pagar:"))
-        error = !(arrayCuotas.some((elem) => elem.cod === cod))
-        while (error) {
-            alert("ERROR, ESE CODIGO NO EXISTE. VUELVA A INTENTARLO")
-            cod = parseInt(prompt("Ingrese la cantidad de cuotas que desea pagar:"))
-            error = !(arrayCuotas.some((elem) => elem.cod === cod))
-        }
-        cuota = arrayCuotas.find((elem) => elem.cod === cod)
-        let precioFinal = cuota.sumaInteres(PrecioEfec)
-        let valorCuota = cuota.calculaCuota(precioFinal, codCuotas)
-        alert("Usted ha elegido " + cuota.descripcion + " con un interes de: " + cuota.interes + "%")
-        console.log("        TICKET B")
-        console.log(" ----------------------------------------------")
-        console.log(" Item  -  DESCRIPCION  -  Cantidad  -  Subtotal")
-        console.log(" " + producto.cod + "      -  " + producto.descripcion + "      -  " + cantidad + "       -  $" + PrecioEfec)
-        console.log(" ")
-        console.log(" ")
-        console.log(" ----------------------------------------------")
-        console.log(" PRECIO FINAL: $" + cuota.sumaInteres(PrecioEfec))
-        console.log(" Financiacion: " + cuota.descripcion + " - Interes: " + cuota.interes + "%")
-        console.log(" Valor de cuota: " + cuota.calculaCuota(cuota.sumaInteres(PrecioEfec)))
-        alert("En consola figura el detalle de su compra.")
-        sigue = prompt("Ingrese 's' para continuar o cualquier letra para finalizar.")
-        if ((sigue == "s") || (sigue == "S")) {
-            repeat = true
-        }
-        else {
-            repeat = false
-        }
 
-    } while (repeat)
-}
+
+
+// alert("Bienvenido usuario al desafio N°2 correspondiente a la clase N°6")
+// alert("Este simulador es ingresar productos en en una lista de precios, luego podra (si desea) generar ventas en cuotas")
+// alert("Agreguemos productos entonces")
+
+// do {
+//     cod = parseInt(prompt("Ingrese el codigo del producto a cargar:"))
+//     error = arrayProductos.some((elem) => elem.cod === cod)
+//     console.log(error)
+//     while (error) {
+//         alert("EL CODIGO YA EXISTE - VUELVA A INTENTARLO")
+//         cod = parseInt(prompt("Ingrese el codigo del producto a cargar:"))
+//         error = arrayProductos.some((elem) => elem.cod === cod)
+//     }
+//     desc = prompt("Ingrese el nombre o descripcion del producto: ")
+//     prec = parseInt(prompt("Ingrese el precio del producto: "))
+//     agregaproducto(arrayProductos, cod, desc, prec)
+//     console.clear()
+//     muestraProd(arrayProductos)
+//     alert("PRODUCTO CARGADO")
+//     sigue = prompt("Ingrese 's' para agregar otro producto o cualquier letra para finalizar.")
+//     if ((sigue == "s") || (sigue == "S")) {
+//         repeat = true
+//     }
+//     else {
+//         repeat = false
+//     }
+// } while (repeat)
+
+// alert("Su lista de precios ha sido cargada, en consola aparece completa.")
+// console.clear()
+// muestraProd(arrayProductos)
+// Continue = prompt("Ingrese la letra S si desea continuar con el programa de ventas.")
+// if ((Continue == "s") || (Continue == "S")) {
+//     agregaCuota(arrayCuotas, 1, "01 Cuota", 0)
+//     agregaCuota(arrayCuotas, 3, "03 Cuotas", 12)
+//     agregaCuota(arrayCuotas, 6, "06 Cuotas", 24)
+//     agregaCuota(arrayCuotas, 9, "09 Cuotas", 36)
+//     agregaCuota(arrayCuotas, 12, "12 Cuotas", 48)
+//     agregaCuota(arrayCuotas, 18, "18 Cuotas", 72)
+//     do {
+//         console.clear()
+//         muestraProd(arrayProductos)
+//         alert("En la consola figura la lista de productos, el codigo y el precio del mismo.")
+//         cod = parseInt(prompt("Ingrese el codigo del producto que desea comprar."))
+//         error = !(arrayProductos.some((elem) => elem.cod === cod))
+//         while (error) {
+//             alert("ERROR, ESE CODIGO NO EXISTE. VUELVA A INTENTARLO")
+//             cod = parseInt(prompt("Ingrese el codigo del producto que desea comprar."))
+//             error = !(arrayProductos.some((elem) => elem.cod === cod))
+//         }
+//         producto = arrayProductos.find((elem) => elem.cod === cod)
+//         alert("Usted ha elegido: " + producto.descripcion + " a un precio de : $" + producto.precio + ".")
+//         cantidad = parseInt(prompt("Ingrese la cantidad que desea:"))
+//         PrecioEfec = (producto.precio * cantidad)
+//         alert("El precio final es de: $" + PrecioEfec + ".")
+//         muestraCuotas(arrayCuotas)
+//         cod = parseInt(prompt("Ingrese la cantidad de cuotas que desea pagar:"))
+//         error = !(arrayCuotas.some((elem) => elem.cod === cod))
+//         while (error) {
+//             alert("ERROR, ESE CODIGO NO EXISTE. VUELVA A INTENTARLO")
+//             cod = parseInt(prompt("Ingrese la cantidad de cuotas que desea pagar:"))
+//             error = !(arrayCuotas.some((elem) => elem.cod === cod))
+//         }
+//         cuota = arrayCuotas.find((elem) => elem.cod === cod)
+//         let precioFinal = cuota.sumaInteres(PrecioEfec)
+//         let valorCuota = cuota.calculaCuota(precioFinal, codCuotas)
+//         alert("Usted ha elegido " + cuota.descripcion + " con un interes de: " + cuota.interes + "%")
+//         console.log("        TICKET B")
+//         console.log(" ----------------------------------------------")
+//         console.log(" Item  -  DESCRIPCION  -  Cantidad  -  Subtotal")
+//         console.log(" " + producto.cod + "      -  " + producto.descripcion + "      -  " + cantidad + "       -  $" + PrecioEfec)
+//         console.log(" ")
+//         console.log(" ")
+//         console.log(" ----------------------------------------------")
+//         console.log(" PRECIO FINAL: $" + cuota.sumaInteres(PrecioEfec))
+//         console.log(" Financiacion: " + cuota.descripcion + " - Interes: " + cuota.interes + "%")
+//         console.log(" Valor de cuota: " + cuota.calculaCuota(cuota.sumaInteres(PrecioEfec)))
+//         alert("En consola figura el detalle de su compra.")
+//         sigue = prompt("Ingrese 's' para continuar o cualquier letra para finalizar.")
+//         if ((sigue == "s") || (sigue == "S")) {
+//             repeat = true
+//         }
+//         else {
+//             repeat = false
+//         }
+
+//     } while (repeat)
+// }
